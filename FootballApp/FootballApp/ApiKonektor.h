e9ec12fc5cfbadd585b77259a2ead7d059bca07f
Mateusz Lipina <lipimat@interia.pl>
2021-04-03 20:19:06 +0200

    SCHOWANY KLUCZ API

--- FootballApp/FootballApp/ApiKonektor.h
+++ FootballApp/FootballApp/ApiKonektor.h
@@ -11,7 +11,7 @@ class ApiKonektor
 private:
 	const std::string URL = "https://api-football-v1.p.rapidapi.com/v3/";
 	const cpr::Header HEADERS = {{ "x-rapidapi-host" , "api-football-v1.p.rapidapi.com"},
-								 {"x-rapidapi-key", "cf3dcfa9d8msh4a6827aa7fa6556p1c7c63jsn39def2ee1fcd"}};
+								 {"x-rapidapi-key", "RAPIDAPI_KEY"}};
 	
 	cpr::Response wyslijGET(std::string requestURL);
 	int getRok();
