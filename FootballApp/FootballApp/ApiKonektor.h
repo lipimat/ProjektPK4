26187df9812957c4ee62da44ff3240267ff5a065
Mateusz Lipina <lipimat@interia.pl>
2021-04-22 00:12:26 +0200

    Znowu chowanie klucza xd

--- FootballApp/FootballApp/ApiKonektor.h
+++ FootballApp/FootballApp/ApiKonektor.h
@@ -15,7 +15,7 @@ class ApiKonektor
 private:
 	const std::string URL = "https://api-football-v1.p.rapidapi.com/v3/";
 	const cpr::Header HEADERS = {{ "x-rapidapi-host" , "api-football-v1.p.rapidapi.com"},
-								 {"x-rapidapi-key", "cf3dcfa9d8msh4a6827aa7fa6556p1c7c63jsn39def2ee1fcd"}};
+								 {"x-rapidapi-key", "RAPIDAPI-KEY"}};
 	
 	cpr::Response wyslijGET(std::string requestURL);
 	int getRok();
